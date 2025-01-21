e8e580659de23ecf976a8396b790a32378e8f6f0
LuisDev01Lab <luisqrja@gmail.com>
2025-01-20 23:28:15 -0500

    feat: Create CountryDataTable component to use in home view

---
+++ src/models/country.table.models.ts
@@ -0,0 +1,6 @@
+export interface Columns {
+  code: string
+  name: string
+  continentName: string
+  actions: string
+}
--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,7 +1,75 @@
+import { Link } from '@tanstack/react-router'
+import { ChevronRight } from 'lucide-react'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+import { CountryDataTable } from '@/components/CountryDataTable'
+
+import type { ColumnDef } from '@tanstack/react-table'
+import type { Columns } from '@/models/country.table.models'
+
+const columns: ColumnDef<Columns>[] = [
+  {
+    accessorKey: 'code',
+    header: 'Code',
+  },
+  {
+    accessorKey: 'name',
+    header: 'Name',
+  },
+  {
+    accessorKey: 'continentName',
+    header: 'Continent',
+  },
+  {
+    accessorKey: 'actions',
+    header: '',
+    cell: ({ row }) => {
+      const data = row.original
+      
+      return (
+        <div className='text-right'>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <Link to="/country/$countryId" params={{ countryId: data.code }}>
+                  <ChevronRight size="18"></ChevronRight>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>See details</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+      )
+    }
+  },
+]
+
+const data = [
+  {
+    code: 'US',
+    name: 'United States',
+    continentName: 'North America',
+    actions: 'Edit Delete',
+  },
+  {
+    code: 'CA',
+    name: 'Canada',
+    continentName: 'North America',
+    actions: 'Edit Delete',
+  },
+]
+
 export function HomeView() {
   return <>
     <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
       Welcome to my Country App
     </h1>
+    <CountryDataTable columns={columns} data={data} />
   </>
 }
