c670d04d0e540e911fd9ab2c70cd5b58ef06707c
LuisDev01Lab <luisqrja@gmail.com>
2025-01-20 21:54:18 -0500

    feat: Config eslint-plugin-react

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,12 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import reactPlugin from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'src/components/ui/**/*'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
@@ -16,6 +17,8 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      ...reactPlugin.configs.flat,
+      ...reactPlugin.configs['jsx-runtime']
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
