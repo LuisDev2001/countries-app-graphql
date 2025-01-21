377b23222df31106edc3f3fd2eaffddcf652731b
LuisDev01Lab <luisqrja@gmail.com>
2025-01-20 21:38:43 -0500

    feat: Config shadcn to work in react

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,9 +1,10 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import { Button } from "@/components/ui/button"
 
 export const Route = createLazyFileRoute('/')({
   component: HomeView,
 })
 
 function HomeView() {
-  return <div>Hello "/"!</div>
+  return <div>Hello "/"! <Button>Hola mundo</Button></div>
 }
